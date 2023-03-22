ff893ef2ffbffd8c51b6ee027547a85b54909a56
hovo-hakobyan <88614889+hovo-hakobyan@users.noreply.github.com>
2023-03-22 19:20:58 +0100

    Added model Summoner

---
+++ Model/Summoner.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DAE15_HovhannesHakobyan_Exam.Model
+{
+    public class Summoner
+    {
+        [JsonProperty("summonerName")]
+        public string Name { get; set; }
+
+        [JsonProperty("leaguePoints")]
+        public int LeaguePoints { get; set; }
+    }
+}
