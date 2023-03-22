d1ebddd9603ce1e545ed4c9503a2897cc930be8b
hovo-hakobyan <88614889+hovo-hakobyan@users.noreply.github.com>
2023-03-23 00:28:43 +0100

    Added subclasses as properties to work with different endpoints

--- Model/Summoner.cs
+++ Model/Summoner.cs
@@ -9,16 +9,44 @@ namespace _2DAE15_HovhannesHakobyan_Exam.Model
 {
     public class Summoner
     {
-        [JsonProperty("summonerName")]
-        public string Name { get; set; }
-
-        [JsonProperty("leaguePoints")]
-        public int LeaguePoints { get; set; }
-
         [JsonProperty("summonerId")]
         public string Id { get; set; }
 
+
         [JsonIgnore]
-        public int Rank { get; set; }
+        public SummonerInfo SummonerInfo { get; set; }
+        [JsonIgnore]
+        public RankInfo RankInfo { get; set; }
+       
+    }
+
+    public class TopSummoner : Summoner
+    {
+        [JsonProperty("leaguePoints")]
+        public int LeaguePoints { get; set; }
+
+        [JsonIgnore]
+        public int LadderRank { get; set; }
+    }
+
+    public class SummonerInfo
+    {
+        [JsonProperty("profileIconId")]
+        public int ProfileIconId { get; set; }
+
+        [JsonProperty("summonerLevel")]
+        public int Level { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+
+    public class RankInfo
+    {
+        [JsonProperty("tier")]
+        public string Tier { get; set; }
+
+        [JsonProperty("rank")]
+        public string Rank { get; set; }
     }
 }
