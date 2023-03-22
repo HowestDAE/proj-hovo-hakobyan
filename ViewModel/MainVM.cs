a9ed0bfabdec3dc28660382186210008615dd309
hovo-hakobyan <88614889+hovo-hakobyan@users.noreply.github.com>
2023-03-22 19:21:16 +0100

    Added Overview page + VM

---
+++ ViewModel/MainVM.cs
@@ -0,0 +1,31 @@
+﻿using _2DAE15_HovhannesHakobyan_Exam.View;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace _2DAE15_HovhannesHakobyan_Exam.ViewModel
+{
+    public class MainVM :ObservableObject
+    {
+        public Page CurrentPage { get; set; }
+        public RelayCommand SwitchPageCommand { get; private set; }
+        public OverviewPage OverviewPage { get; set; }
+
+        public MainVM()
+        {
+            SwitchPageCommand = new RelayCommand(SwitchPage);
+            OverviewPage = new OverviewPage();
+            CurrentPage = OverviewPage;
+        }
+
+        private void SwitchPage()
+        {
+
+        }
+    }
+}
---
+++ ViewModel/OverviewVM.cs
@@ -0,0 +1,15 @@
+﻿using _2DAE15_HovhannesHakobyan_Exam.Model;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DAE15_HovhannesHakobyan_Exam.ViewModel
+{
+    public class OverviewVM : ObservableObject
+    {
+        public List<Summoner> Summoners { get; set; }
+    }
+}
