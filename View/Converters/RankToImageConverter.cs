b2a87a1854728b3e99c2abd39eb5f43b06b6f1c7
hovo-hakobyan <88614889+hovo-hakobyan@users.noreply.github.com>
2023-03-24 16:27:12 +0100

    Added rank to image converter + resources for rank icons

---
+++ View/Converters/RankToImageConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace _2DAE15_HovhannesHakobyan_Exam.View.Converters
+{
+    internal class RankToImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string rank = value.ToString().ToLower();
+            return new BitmapImage(new Uri($"pack://application:,,,/Resources/RankedEmblem/emblem-{rank}.png", UriKind.Absolute));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
